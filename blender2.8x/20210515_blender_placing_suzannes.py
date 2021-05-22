a83849c2a398c673b5f30a3813063380b5d5dfda
aoeth <aoeth@yahoo.com>
2021-05-22 16:25:41 +0900

    速度改善

--- blender2.8x/20210515_blender_placing_suzannes.py
+++ blender2.8x/20210515_blender_placing_suzannes.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import math
 import random
 from typing import List, Tuple
 
 import bpy
 from bpy.types import Collection, Object, Scene
+from mathutils import Euler, Matrix, Vector
 from mathutils.bvhtree import BVHTree
 
 """
@@ -17,11 +20,40 @@ from mathutils.bvhtree import BVHTree
 
 # 定数
 BASE_OBJ_NAME = 'base_obj'  # ベースオブジェクト名
-COPIES = 100  # 複製回数
+COPIES = 20  # 複製回数
 PLACING_AREA_RANGE = [5, 5, 10]  # コピーしたオブジェクトを配置する範囲
 Z_OFFSET = 2  # コピーしたオブジェクトを配置する範囲のうち、Z座標の最低値
 COLLECTION_OF_COPIED = 'copied'  # コピーしたオブジェクトの格納用コレクション名
 MAX_TRYIALS = 10  # 1複製オブジェクトのランダム配置の最大試行回数
+
+
+class ObjWithBvh:
+    """
+    blenderのオブジェクト+BVH
+    作成時にBVH計算して保持
+    """
+
+    def __init__(self, obj: Object, location=[0.0, 0.0, 0.0], rotation=[0.0, 0.0, 0.0]) -> None:
+        self.obj = obj
+        self.bvh = ObjWithBvh._create_bvh(obj, location, rotation)
+
+    @staticmethod
+    def _create_bvh(obj: Object, location, rotation):
+        """
+        参考：https://katsumi3.hatenablog.com/entry/2020/03/20/183925
+        Blenderオブジェクト+位置情報+回転情報からBVH作成する
+        シーンにリンクしてなくてOK
+        拡大は非対応なので必要であれば実装する
+        """
+
+        loc_matrix = Matrix.Translation(location)
+        rot_matrix = Euler(rotation).to_matrix().to_4x4()
+
+        mat = obj.matrix_world @ loc_matrix @ rot_matrix
+        vert = [mat @ v.co for v in obj.data.vertices]
+        poly = [p.vertices for p in obj.data.polygons]
+        bvh = BVHTree.FromPolygons(vert, poly)
+        return bvh
 
 
 def crear_collection(collection_name: str, scene: Scene) -> Collection:
@@ -66,43 +98,27 @@ def get_random_rotation() -> List[float]:
     return [x_val, y_val, z_val]
 
 
-def is_overlap_two(obj1: Object, obj2: Object) -> bool:
+def is_overlap_two(obj1: ObjWithBvh, obj2: ObjWithBvh) -> bool:
     """
     2オブジェクトが重なってないかチェックする
-    毎回BVHTree作ってるのはどう考えても効率悪いけど
-    死ぬほど遅くなければこのままでいい…
     参考：https://blender.stackexchange.com/questions/149891/python-in-blender-2-8-testing-if-two-objects-overlap-in-the-xy-plane/150014
     """
-    # Get their world matrix
-    mat1 = obj1.matrix_world
-    mat2 = obj2.matrix_world
-
-    # Get the geometry in world coordinates
-    vert1 = [mat1 @ v.co for v in obj1.data.vertices]
-    poly1 = [p.vertices for p in obj1.data.polygons]
-
-    vert2 = [mat2 @ v.co for v in obj2.data.vertices]
-    poly2 = [p.vertices for p in obj2.data.polygons]
-
-    # Create the BVH trees
-    bvh1 = BVHTree.FromPolygons(vert1, poly1)
-    bvh2 = BVHTree.FromPolygons(vert2, poly2)
 
     # -> bool:にしておけばboolにcastされるのか？よくわからないのでTrueFalseで返す
-    if bvh1.overlap(bvh2):
+    if obj1.bvh.overlap(obj2.bvh):
         return True
     else:
         return False
 
 
-def is_overlap_list(obj: Object, compare_objs: Tuple[Object, ...]) -> bool:
+def is_overlap_list(obj: ObjWithBvh, compare_objs: Tuple[ObjWithBvh, ...]) -> bool:
     """
     あるオブジェクトと、タプル中のオブジェクトが重なってないかチェックする
     """
     result = False
 
     for compare_obj in compare_objs:
-        if obj.name == compare_obj.name:
+        if obj.obj.name == compare_obj.obj.name:
             continue
 
         if is_overlap_two(obj, compare_obj):
@@ -122,23 +138,31 @@ def main():
     # 複製オブジェクト格納用コレクション準備
     copied_collection = crear_collection(COLLECTION_OF_COPIED, scene)
 
+    # bvh付きは別に準備
+    copieds = []
+
     # コピーしてランダムに配置
     for i in range(0, COPIES):
         copied_obj: Object = base_obj.copy()
         copied_obj.name = f"copied_{i:03d}"
-
         trial = 1
 
         # ランダム座標＆回転で配置してみて、重なってなければ採用（break）
         while trial <= MAX_TRYIALS:
-            copied_obj.location = get_random_location(
-                PLACING_AREA_RANGE, Z_OFFSET)
-            copied_obj.rotation_euler = get_random_rotation()
-            copied_collection.objects.link(copied_obj)  # 絶対このあたり遅くなってる
-            bpy.context.view_layer.update()  # 強制再描画…
-            if not is_overlap_list(copied_obj, copied_collection.all_objects):
+
+            location = get_random_location(PLACING_AREA_RANGE, Z_OFFSET)
+            copied_obj.location = location
+
+            rotation = get_random_rotation()
+            copied_obj.rotation_euler = rotation
+
+            # この時点でlinkしてないとBVH計算時に位置や回転を適用する必要ありなので渡す
+            copied_obj_with_bvh = ObjWithBvh(copied_obj, location, rotation)
+
+            if not is_overlap_list(copied_obj_with_bvh, copieds):
+                copied_collection.objects.link(copied_obj)
+                copieds.append(copied_obj_with_bvh)
                 break
-            copied_collection.objects.unlink(copied_obj)
             trial += 1
 
         # だめなときは諦める
@@ -146,15 +170,19 @@ def main():
             print(f"置けなかったので諦めた {i:03d}個目 試行回数:{trial - 1}")
             bpy.data.objects.remove(copied_obj)
 
-    # リジッドボディを複製オブジェクトに1つ1つ追加
-    for rigid_obj in copied_collection.all_objects:
-        rigid_obj.select_set(True)  # 画面上でやるのと同じ操作なのが気に入らない…
-        bpy.context.view_layer.objects.active = rigid_obj
-        bpy.ops.rigidbody.objects_add()
-        rigid_obj.select_set(False)
+    # アクティブからコピーすればいいので不要　一応残しとく
+    # # リジッドボディを複製オブジェクトに1つ1つ追加
+    # for rigid_obj in copied_collection.all_objects:
+    #     rigid_obj.select_set(True)  # 画面上でやるのと同じ操作なのが気に入らない…
+    #     bpy.context.view_layer.objects.active = rigid_obj
+    #     bpy.ops.rigidbody.objects_add()
+    #     rigid_obj.select_set(False)
 
     print(f"{len(copied_collection.objects)}個置けた")
 
 
+# ifなくても良いけど
 if __name__ == "__main__":
+    main()
+else:
     main()
